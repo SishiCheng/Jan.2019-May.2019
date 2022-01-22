e49edb73bb1190975205ac75672e4015fa3ba918
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-01-22 00:54:41 -0500

    Add files via upload

---
+++ final quicksort/cmake-build-debug/CMakeFiles/final_quicksort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/final_quicksort.dir/main.cpp.obj"
+  "final_quicksort.pdb"
+  "final_quicksort.exe"
+  "final_quicksort.exe.manifest"
+  "libfinal_quicksort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/final_quicksort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ final quicksort/main.cpp
@@ -0,0 +1,47 @@
+// This program demonstrates the QuickSort Algorithm.
+#include <iostream>
+
+using namespace std;
+// Function prototypes
+
+int main() {
+    const int size=10;// Array size
+
+    int counter=0;// Loop counter
+    int array[size] = {7, 3, 9, 2, 0, 1, 8, 4, 6, 5};
+
+    for (int i=0; i<10; i++) {
+        cout << array[i] << " ";
+    }// Display the array contents.
+
+    // Sort the array.
+
+    // Display the array contents.
+    return 0;
+}
+
+//************************************************
+// quickSort uses the quicksort algorithm to *
+// sort set, from set[start] through set[end]. *
+//************************************************
+
+void quickSort() {
+    // Get the pivot point.
+
+    // Sort the first sublist.
+
+    // Sort the second sublist.
+}
+
+
+//***********************************************************
+// partition selects the value in the middle of the *
+// array set as the pivot. The list is rearranged so *
+// all the values less than the pivot are on its left*
+// and all the values greater than pivot are on its right. *
+//***********************************************************
+
+//******************************************
+// swap simply exchanges the contents of *
+// value1 and value2. *
+//******************************************
