d7183782d2efa96c5dad0f47f990018e0a22230e
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-01-22 00:42:15 -0500

    Add files via upload

---
+++ array/cmake-build-debug/CMakeFiles/array.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/array.dir/main.cpp.obj"
+  "CMakeFiles/array.dir/main.cpp.obj.d"
+  "array.exe"
+  "array.exe.manifest"
+  "array.pdb"
+  "libarray.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
